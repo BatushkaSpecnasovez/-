cc404aea8278549ede4ec9021f4bf87a5038a226
BatushkaSpecnasovez <71442741+BatushkaSpecnasovez@users.noreply.github.com>
2021-01-09 18:48:01 +0300

    Add files via upload

---
+++ 14 задание/4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    printf("Введите число P\n");
+    scanf_s("%i", &n);
+    int k = 1;
+    float s = 1000;
+    while (s <= 1100) {
+        ++k;
+        s += s * n / 100;
+    }
+    printf("k = %i", s);
+    return 0;
+}
---
+++ 14 задание/6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    printf("Введите целое число N\n");
+    scanf_s("%i", &n);
+    int f1 = 1, f2 = 1, f = 0, k = 2;
+    while (f < n) {
+        ++k;
+        f = f2 + f1;
+        f2 = f1;
+        f1 = f;
+    }
+    cout << "k:" << k;
+    return 0;
+}
