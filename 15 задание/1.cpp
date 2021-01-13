ddb006370f31b265a02cd107a09fd1b36b0e718e
BatushkaSpecnasovez <71442741+BatushkaSpecnasovez@users.noreply.github.com>
2021-01-13 16:10:49 +0300

    Add files via upload

---
+++ 15 задание/1.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdio>
+#include <locale.h>
+void poweraA3(float a, float* b)
+{
+    *b = a * a * a;
+}
+
+int main(void)
+{
+    int i;
+    setlocale(LC_ALL, "Rus");
+    for (i = 1;i <= 5;++i) {
+        float a;
+        printf("Введите число \A:");
+        scanf_s("%f", &a);
+        float b;
+        poweraA3(a, &b);
+        printf("B: %f\n", b);
+    }
+    return 0;
+}
